afcae69dd365b100f5fee10846425f4a1e45b294
Pathik Shah <82212939+pathik455@users.noreply.github.com>
2023-03-16 20:09:27 +0530

    Add files via upload

---
+++ client/src/Post.js
@@ -0,0 +1,24 @@
+import {Link} from "react-router-dom";
+import { formatISO9075 } from "date-fns"; //created post at time
+
+export default function Post({_id, title, summary, content, cover, createdAt, author}){
+    return (
+        <div className="post">
+          <div className="image">
+            <Link to={`/post/${_id}`}>
+                <img src={'http://localhost:4000/'+cover} alt=""></img>
+            </Link>
+          </div>
+          <div className="texts">
+            <Link to={`/post/${_id}`}>
+                <h2>{title}</h2>
+            </Link>
+            <p className="info">
+              <a className="author">{author.username}</a>
+              <time>{formatISO9075(new Date(createdAt))}</time>
+            </p>
+            <p className="summary">{summary}</p>
+          </div>
+        </div>
+    );
+}
